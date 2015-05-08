65f8cf6562fc7e5046d76eb4f02793633fe0467e
Lauren Bricker <Lauren.Bricker@BRICKER468.lakesideschool.org>
2015-05-07 20:46:09 -0700

    Initial version

---
+++ Hello.java
@@ -0,0 +1,24 @@
+// Lauren Bricker
+// Example 1
+// This is an example first program for the students to type in
+
+// Hello.java
+// Version 1.0 
+// September 5, 2008
+
+public class Hello 
+{ 
+  
+  /** This is the entry point into the program, where it all begins
+    *  
+    *  @param args  the command line arguments that are passed into the program
+    *  
+    */
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello World!");
+    
+  }
+  
+  
+}
